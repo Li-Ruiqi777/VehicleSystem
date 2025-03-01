64f595d8301dba2c2cc0fbc14c48edee82e2bfb8
Li-Ruiqi <1009962019@qq.com>
2025-03-01 17:02:06 +0800

    3.1：重写了一下LED驱动，更加规范一些

--- src/driver/LED/LED_driver.c
+++ src/driver/LED/LED_driver.c
@@ -2,6 +2,7 @@
 
 #include "linux/init.h"
 #include "linux/kern_levels.h"
+#include "linux/kernel.h"
 #include "linux/module.h"
 
 #include "linux/cdev.h"
@@ -14,7 +15,8 @@
 #define LEDOFF 0
 #define LEDON  1
 
-struct LED_dev
+// 设备驱动的信息
+struct LEDInfo
 {
     dev_t devid;                          // 设备号
     int major;                            // 主设备号
@@ -26,30 +28,10 @@ struct LED_dev
     int gpio_index;                       // led对应gpio的序号
 };
 
-static struct LED_dev led_dev;
-
-void led_gpio_init(struct platform_device *pdev);
-static int led_open(struct inode *inode, struct file *file);
-static int led_release(struct inode *inode, struct file *file);
-static ssize_t led_read(struct file *file, char __user *buf, size_t count, loff_t *ppos);
-static ssize_t led_write(struct file *file, const char __user *buf, size_t count, loff_t *ppos);
-static int led_probe(struct platform_device *dev);
-static int led_remove(struct platform_device *dev);
-static int led_module_init(void);
-static void led_module_exit(void);
-
-static struct file_operations fops = {
-    .owner = THIS_MODULE,
-    .open = led_open,
-    .release = led_release,
-    .read = led_read,
-    .write = led_write,
-};
-
 static int led_open(struct inode *inode, struct file *file)
 {
     printk(KERN_INFO "LED opened\n");
-    file->private_data = &led_dev;
+    file->private_data = container_of(inode->i_cdev, struct LEDInfo, cdev);
     return 0;
 }
 
@@ -69,89 +51,105 @@ static ssize_t led_write(struct file *file, const char __user *buf, size_t count
     if (buf == NULL)
         return -EINVAL;
 
-    int ret = 0;
+    struct LEDInfo *led_info = file->private_data;
     uint8_t databuf[1];
-    ret = __copy_from_user(databuf, buf, count);
-    struct LED_dev *dev = file->private_data;
 
-    if (ret < 0)
+    if (__copy_from_user(databuf, buf, count) < 0)
     {
         printk(KERN_ERR "write LED status failed! \n");
         return EFAULT;
     }
 
+    printk(KERN_INFO "intput data = %d \n", databuf[0]);
+
     if (databuf[0] == LEDON)
-        gpio_set_value(dev->gpio_index, 0);
+        gpio_set_value(led_info->gpio_index, 0);
 
     else
-        gpio_set_value(dev->gpio_index, 1);
+        gpio_set_value(led_info->gpio_index, 1);
+
+    return count;
 }
+
+static struct file_operations fops = {
+    .owner = THIS_MODULE,
+    .open = led_open,
+    .release = led_release,
+    .read = led_read,
+    .write = led_write,
+};
 
 void led_gpio_init(struct platform_device *pdev)
 {
+    struct LEDInfo *led_info = platform_get_drvdata(pdev);
+
     // 获取设备树
-    led_dev.device_tree_node = pdev->dev.of_node;
-    if (led_dev.device_tree_node == NULL)
+    led_info->device_tree_node = pdev->dev.of_node;
+    if (led_info->device_tree_node == NULL)
     {
         printk("gpioled node nost find!\r\n");
         return;
     }
 
     // 从设备树中获取GPIO编号
-    led_dev.gpio_index = of_get_named_gpio(led_dev.device_tree_node, "led-gpio", 0);
-    if (led_dev.gpio_index < 0)
+    led_info->gpio_index = of_get_named_gpio(led_info->device_tree_node, "led-gpio", 0);
+    if (led_info->gpio_index < 0)
     {
         printk("can't get led-gpio! \r\n");
         return;
     }
-    printk("LED's GPIO Index = %d\r\n", led_dev.gpio_index);
+    printk("LED's GPIO Index = %d\r\n", led_info->gpio_index);
 
     // 申请并配置GPIO
-    gpio_request(led_dev.gpio_index, "led0");
-    gpio_direction_output(led_dev.gpio_index, 1);
+    gpio_request(led_info->gpio_index, "led0");
+    gpio_direction_output(led_info->gpio_index, 1);
 }
 
 int led_probe(struct platform_device *pdev)
 {
+    struct LEDInfo *led_info = devm_kzalloc(&pdev->dev, sizeof(struct LEDInfo), GFP_KERNEL);
+    // 将设备信息保存到platform_device的上下文中, 以便在其他地方能够访问到
+    platform_set_drvdata(pdev, led_info);
+
     led_gpio_init(pdev);
 
     // 动态分配设备号
-    if (led_dev.major)
+    if (led_info->major)
     {
-        led_dev.devid = MKDEV(led_dev.major, 0);
-        register_chrdev_region(led_dev.devid, 1, "gpio_led");
+        led_info->devid = MKDEV(led_info->major, 0);
+        register_chrdev_region(led_info->devid, 1, "gpio_led");
     }
     else
-    {                                                          /* 没有定义设备号 */
-        alloc_chrdev_region(&led_dev.devid, 0, 1, "gpio_led"); /* 申请设备号 */
-        led_dev.major = MAJOR(led_dev.devid);                  /* 获取分配号的主设备号 */
-        led_dev.minor = MINOR(led_dev.devid);                  /* 获取分配号的次设备号 */
+    {
+        alloc_chrdev_region(&led_info->devid, 0, 1, "gpio_led"); // 申请设备号
+        led_info->major = MAJOR(led_info->devid);                  // 获取分配号的主设备号
+        led_info->minor = MINOR(led_info->devid);                  // 获取分配号的次设备号
     }
-    printk("gpioled major=%d,minor=%d\r\n", led_dev.major, led_dev.minor);
+    printk("gpioled major=%d,minor=%d\r\n", led_info->major, led_info->minor);
 
     // 注册字符设备
-    led_dev.cdev.owner = THIS_MODULE;
-    cdev_init(&led_dev.cdev, &fops);
-    int ret = cdev_add(&led_dev.cdev, led_dev.devid, 1);
+    led_info->cdev.owner = THIS_MODULE;
+    cdev_init(&led_info->cdev, &fops);
+    int ret = cdev_add(&led_info->cdev, led_info->devid, 1); // 将字符设备和设备号关联, 并插入内核
     if (ret)
     {
         printk(KERN_NOTICE "Error %d adding gpio_led", ret);
-        unregister_chrdev_region(led_dev.devid, 1);
+        unregister_chrdev_region(led_info->devid, 1);
         return ret;
     }
 
-    // 创建设备类
-    led_dev.class = class_create(THIS_MODULE, "gpio_led");
-    if (IS_ERR(led_dev.class))
+    // 在/sys/class下创建设备类
+    led_info->class = class_create(THIS_MODULE, "gpio_led");
+    if (IS_ERR(led_info->class))
     {
-        return PTR_ERR(led_dev.class);
+        return PTR_ERR(led_info->class);
     }
 
-    // 创建设备节点
-    led_dev.device = device_create(led_dev.class, NULL, led_dev.devid, NULL, "led");
-    if (IS_ERR(led_dev.device))
+    // 在/dev下创建设备节点,并关联到设备类
+    led_info->device = device_create(led_info->class, NULL, led_info->devid, NULL, "led");
+    if (IS_ERR(led_info->device))
     {
-        return PTR_ERR(led_dev.device);
+        return PTR_ERR(led_info->device);
     }
 
     printk(KERN_INFO "gpio_led module loaded \n");
@@ -161,11 +159,13 @@ int led_probe(struct platform_device *pdev)
 
 static int led_remove(struct platform_device *pdev)
 {
-    device_destroy(led_dev.class, led_dev.devid);
-    class_destroy(led_dev.class);
-    cdev_del(&led_dev.cdev);
-    devm_gpio_free(&pdev->dev, led_dev.gpio_index);
-    unregister_chrdev_region(led_dev.devid, 1);
+    struct LEDInfo *led_info = platform_get_drvdata(pdev);
+
+    device_destroy(led_info->class, led_info->devid);
+    class_destroy(led_info->class);
+    cdev_del(&led_info->cdev);
+    devm_gpio_free(&pdev->dev, led_info->gpio_index);
+    unregister_chrdev_region(led_info->devid, 1);
 
     printk(KERN_INFO "gpio_led module unloaded \n");
 }
